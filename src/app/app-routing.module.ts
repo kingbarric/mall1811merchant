4bba2bb64f3676c1d81e556e6ede7bfc81c956e5
kingbarric <mac@Macs-MBP.mshome.net>
2019-09-30 18:14:15 +0100

    after login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
   {path: '',   redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'login',   component:LoginComponent},
   {path: 'dashboard', component: DashboardComponent,
- // canActivate: [AuthGuard],
- 
+ // canActivate: [AuthGuard], 
   loadChildren: () => import('./dashboard/dashboard.module').then(mod => mod.DashboardModule)
 
 }
